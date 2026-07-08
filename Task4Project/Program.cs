ce2cb734bc49c2e9c7863de9a2569c14736c8d67
itsriima <itsriima2442@gmail.com>
2026-07-08 16:36:41 +0300

    Implement Task2

--- Task4Project/Program.cs
+++ Task4Project/Program.cs
@@ -9,11 +9,25 @@
             Console.WriteLine("Enter your name:");
             string userName = Console.ReadLine();
             PrintWelcome(userName);
+            //Task 2 - Square Number Function
+            ///////////////////////////////////////////
+            Console.WriteLine("Enter a number:");
+            int number = int.Parse(Console.ReadLine());
+
+            int result = Square(number);
+
+            Console.WriteLine("Square = " + result);
+
         }
         //1
         static void PrintWelcome(string name)
         {
             Console.WriteLine("Welcome, " + name + "!");
+        }
+        //2
+        static int Square(int number)
+        {
+            return number * number;
         }
 
     }
