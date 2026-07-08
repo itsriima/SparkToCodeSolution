9de22f60de4f63e60dc70dceea158d60bad2ecfa
itsriima <itsriima2442@gmail.com>
2026-07-08 15:37:12 +0300

    Implement Task1

--- Task1Project/Program.cs
+++ Task1Project/Program.cs
@@ -4,7 +4,13 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //1- Personal Info Card
+            //////////////////////////////////////
+            string name = "Rima ";
+            int age = 20;
+            double height = 1.55;
+            bool student = true;
+            Console.WriteLine("Name: " + name + "Age:" + age + "Height: " + height + "Student: " + student);
         }
     }
 }
