4d79c78ed51f364dec5e5c578b3ed7d0579547cc
itsriima <itsriima2442@gmail.com>
2026-07-08 16:16:22 +0300

    Implement Task1

--- Task3Project/Program.cs
+++ Task3Project/Program.cs
@@ -4,7 +4,16 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Task 1 - Absolute Difference
+            /////////////////////////////
+            Console.WriteLine("Enter the first number:");
+            int firstNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the Second number:");
+            int secondNumber = int.Parse(Console.ReadLine());
+            int difference = firstNumber - secondNumber;
+            int positiveDifference = Math.Abs(difference);
+            Console.WriteLine("Absolute Difference: " + positiveDifference);
+
         }
     }
 }
