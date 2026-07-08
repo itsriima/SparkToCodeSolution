42b76c46717e6b8ef820b8335613b6dd44731388
itsriima <itsriima2442@gmail.com>
2026-07-08 16:34:05 +0300

    Imlement Task1

---
+++ Task4Project/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Task4Project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Task 1 - Personalized Welcome Function
+            ///////////////////////////////////////////
+            Console.WriteLine("Enter your name:");
+            string userName = Console.ReadLine();
+            PrintWelcome(userName);
+        }
+        //1
+        static void PrintWelcome(string name)
+        {
+            Console.WriteLine("Welcome, " + name + "!");
+        }
+
+    }
+}
