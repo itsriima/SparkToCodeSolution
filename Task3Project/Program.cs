2eb7d7a053c359a1afe8cc5f3a71bb761e61e93b
itsriima <itsriima2442@gmail.com>
2026-07-08 16:15:50 +0300

    Add File To Solv Task3

---
+++ Task3Project/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Task3Project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
