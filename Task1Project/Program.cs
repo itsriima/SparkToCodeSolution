28741e3757f583211af6b86f2b6bf683defdb937
itsriima <itsriima2442@gmail.com>
2026-07-08 14:50:33 +0300

    add new Project

---
+++ Task1Project/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Task1Project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
