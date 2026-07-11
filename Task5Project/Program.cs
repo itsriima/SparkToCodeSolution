f129e8134b1ac6601ec794e0cc724125af702512
itsriima <itsriima2442@gmail.com>
2026-07-11 13:44:20 +0300

    add File To Solve Task5

---
+++ Task5Project/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Task5Project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
