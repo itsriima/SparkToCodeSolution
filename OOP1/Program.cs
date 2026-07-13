5799bb6d1357a56c15d74435b95b2373e8fde6bd
itsriima <itsriima2442@gmail.com>
2026-07-13 09:07:57 +0300

    Implement Task1

--- OOP1/Program.cs
+++ OOP1/Program.cs
@@ -4,7 +4,50 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+        }
+        //1- class BankAccount
+        /////////////////////////////
+        public class BankAccount
+        {
+            // Properties
+            public int AccountNumber { get; set; }
+            public string HolderName { get; set; }
+            public double Balance { get; set; }
+
+            // Methods
+            public void Deposit(double amount)
+            {
+                Balance += amount;
+                SendEmail();
+            }
+
+            private void SendEmail()
+            {
+                // Placeholder for email notification
+            }
+
+            public void Withdraw(double amount)
+            {
+                if (Balance >= amount)
+                {
+                    Balance -= amount;
+                }
+
+                SendEmail();
+            }
+
+            public double CheckBalance()
+            {
+                PrintInformation();
+                return Balance;
+            }
+
+            private void PrintInformation()
+            {
+                Console.WriteLine("Holder Name: " + HolderName);
+                Console.WriteLine("Balance: " + Balance);
+            }
         }
     }
-}
+    }
+
